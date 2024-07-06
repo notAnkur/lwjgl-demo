731110a864eb1ab4d373447c2499c04b228ca2e6
Ankur Anant <ankuranant99@gmail.com>
2024-07-07 03:29:15 +0530

    basic window

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,44 @@
 package main;
 
-public class Main {
+import engine.io.Window;
 
+public class Main implements Runnable {
+
+	public Thread game;
+	public static Window window;
+	public static final int WIDTH = 1280, HEIGHT = 760;
+	
+	public void start() {
+		game = new Thread(this, "game");
+		game.start();
+	}
+	
+	public static void init() {
+		System.out.println("Initializing game");
+		window = new Window(WIDTH, HEIGHT, "Game");
+		window.create();
+	}
+	
+	public void run() {
+		init();
+		while(!window.shouldClose()) {
+			update();
+			render();
+		}
+	}
+	
+	private void update() {
+//		System.out.println("Updating game");
+		window.update();
+	}
+	
+	private void render() {
+//		System.out.println("Rendering game");
+		window.swapBuffers();
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new Main().start();
 	}
 
 }
